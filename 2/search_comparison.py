84909fc4ba99fe740331541dec51184c5574d4e9
Константин <kostiktell@mail.ru>
2025-09-15 16:11:40 +0300

    Добавлена точка входа программы с выполнением основных функций

--- 2/search_comparison.py
+++ 2/search_comparison.py
@@ -1,6 +1,5 @@
 import time
 
-
 def linear_search(arr, target):
     for i in range(len(arr)):  # O(n)
         if arr[i] == target:   # O(1)
@@ -33,4 +32,19 @@ def measure_time(search_func, arr, target, runs=5):
         times.append(end - start)    
     return sum(times) / runs    # считает среднее время выполнения    
 
+if __name__ == '__main__':
+
+    sizes = [1000, 2000, 5000, 10000, 50000, 
+             100000, 500000, 1000000, 5000000, 10000000] # массив размеров
+
+    for size in sizes:
+        arr = list(range(size))  # создаём отсортированный массив
+        targets = [arr[0], arr[-1], arr[size // 2], size + 1]  
+        # первый, последний, средний, отсутствующий элемент
+
+        print(f"\n--- Размер массива: {size} ---")
+        for target in targets:
+            lin_time = measure_time(linear_search, arr, target)
+            bin_time = measure_time(binary_search, arr, target)
+            print(f"Цель: {target} | Linear: {lin_time:.8f} c | Binary: {bin_time:.8f} c")
 
